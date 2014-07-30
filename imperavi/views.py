d6e890c2dfd31b1fa161c6ffa7d5951c7ce4c95a
mpachas <miguel.pachas.garcia@gmail.com>
2014-07-30 11:10:31 +0200

    full name

--- imperavi/views.py
+++ imperavi/views.py
@@ -87,8 +87,10 @@ def upload_file(request, upload_path=None, upload_link=None):
     form = FileForm(request.POST, request.FILES)
     if form.is_valid():
         uploaded_file = form.cleaned_data['file']
+        file_name, extension = os.path.splitext(uploaded_file.name)
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        path = os.path.join(upload_path or UPLOAD_PATH, uploaded_file.name+timestr)
+        full_name = '{0}{1}'.format(file_name+timestr, extension)
+        path = os.path.join(upload_path or UPLOAD_PATH, full_name)
         image_path = default_storage.save(path, uploaded_file)
         image_url = default_storage.url(image_path)
         if upload_link:
